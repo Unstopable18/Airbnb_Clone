54b923be0b964066e6526dbd5ce8ef58e956a4ea
Vaishnavi Deshmukh <117887492+VDeshmukhCemtrex@users.noreply.github.com>
2023-12-22 16:23:46 +0530

    Image added with tab in listingView

--- Airbnb Clone/Core/Listings/ListingItemView.swift
+++ Airbnb Clone/Core/Listings/ListingItemView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    var images = [
+        "image-1",
+        "image-2",
+        "image-3",
+        "image-4",
+        "image-5",
+    ]
     var body: some View {
         VStack(spacing: 8){
             // Images
-            Rectangle()
-                .frame(height:320)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+            TabView{
+                ForEach(images, id: \.self){ image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+            .frame(height:320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+            
             // Listing Details
             HStack(alignment: .top){
                 
@@ -42,7 +57,7 @@ struct ListingItemView: View {
             }
             .font(.footnote)
         }
-
+        
     }
 }
 
